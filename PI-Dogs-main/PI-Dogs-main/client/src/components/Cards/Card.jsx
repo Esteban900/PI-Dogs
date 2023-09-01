b928d1a4eb1f84f47e0284c8c7a685837470b015
Esteban900 <estebanandrusyszyn90@gmail.com>
2023-09-01 19:03:44 -0300

    edit css

--- PI-Dogs-main/PI-Dogs-main/client/src/components/Cards/Card.jsx
+++ PI-Dogs-main/PI-Dogs-main/client/src/components/Cards/Card.jsx
@@ -8,12 +8,12 @@ const Card = (props) => {
         <div className={ style.Card}>
             {/* <p className={ style.title}>id: {props.id}</p> */}
             <Link to={`/detail/${props.id}`}>
-            <p className={ style.title}>name: {props.name}</p>
+            <p className={ style.title}>{props.name}</p>
             </Link>
             <img src={props.image} alt="img not found" className={ style.cardImage}/>
-            <p className={ style.title}> weight: {props.weight_min}kg - {props.weight_max}kg</p>
+            <p className={ style.text}> weight: {props.weight_min}kg - {props.weight_max}kg</p>
             {/* <p className={ style.title}> weight max: {props.weight_max}</p> */}
-            <p className={ style.title}>{props.temperament.join(", ")}</p>
+            <p className={ style.text}>{props.temperament.join(", ")}</p>
         
         </div>
     )
